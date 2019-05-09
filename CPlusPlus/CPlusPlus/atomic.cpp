a8f655282306353d76df8096060ff7e51555de45
songliao <songliao@tencent.com>
2019-05-09 14:49:07 +0800

    up

--- CPlusPlus/CPlusPlus/atomic.cpp
+++ CPlusPlus/CPlusPlus/atomic.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
+/**
+ * @file atomic.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-05-09
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include <atomic>
+#include <iostream>
+#include <thread>
 #include <memory>
 
 /**
  * [alignof](alignof.md)
  */
 
-using namespace std;
+std::atomic_llong total {0};
+
+void func() {
+    for (size_t i = 0; i < 100000000; i++) {
+        total += i;
+    }
+}
 
 int main() {
-    cout << "HI" << endl;
-    
-    atomic<bool> a;
+    std::thread t1(func);
+    std::thread t2(func);
+    t1.join();
+    t2.join();
+    std::cout << total << std::endl;
     return 0;
 }
