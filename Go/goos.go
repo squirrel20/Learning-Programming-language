31d5632b9f7b770df386d73ed6a7e40699dfe50d
songliao <songliao@tencent.com>
2019-07-04 19:34:10 +0800

    up

--- Go/goos.go
+++ Go/goos.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var goos string = os.Getenv("GOOS")
-    fmt.Printf("The operating system is: %s\n", goos)
-    path := os.Getenv("PATH")
-    fmt.Printf("Path is %s\n", path)
+	goos := os.Getenv("GOOS")
+	fmt.Printf("The operating system is: %s\n", goos)
+	path := os.Getenv("PATH")
+	fmt.Printf("Path is %s\n", path)
 }
